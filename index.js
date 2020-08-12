f09f6a2160adc4d6f85468f383b97a9a631a3bae
Mislav Srecec <mislav.srecec3@gmail.com>
2020-08-12 11:30:36 +0200

    Simple Node Example

---
+++ index.js
@@ -0,0 +1,22 @@
+let rect = {
+  perimiter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+solveRect = (l, b) => {
+  console.log(`Solving for rectangle with l = ${l} and b = ${b}`);
+
+  if (l <= 0 || b <= 0) {
+    console.log(
+      `Rectangle dimensions should be greater than zero: l = ${l} and b = ${b}`
+    );
+  } else {
+    console.log(`The area of the rectangle is ${rect.area(l, b)}`);
+    console.log(`The perimiter of the rectangle is ${rect.perimiter(l, b)}`);
+  }
+};
+
+solveRect(2, 4);
+solveRect(3, 5);
+solveRect(0, 5);
+solveRect(-3, 5);
